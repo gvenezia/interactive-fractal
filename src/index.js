971a0852c54b5e24a00df293f9cf9a7a1dc08c49
gvenezia <g.venezia@outlook.com>
2018-05-06 23:40:17 +0000

    Fix React.component typo and sway and sprout to TreeBox

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 // Import pre-set components from React Armory tutorial
-import { getBoxStyle } from './PythagorasTree.js';
+import { TreeBox } from './PythagorasTree.js';
 import { Timer } from './Timer.js';
 
 // =============================================================
-class AnimatedTree extends React.component {
+class AnimatedTree extends React.Component {
   constructor(props) {
     super(props);
     
@@ -47,10 +47,10 @@ class AnimatedTree extends React.component {
     
     return React.createElement('div', {},
       React.createElement(TreeBox, {
-        totalLevels: 6,
-        size: 100,
-        heightFactor: 0.2 + 0.1*Math.sin(this.state.time),
-        lean: 0.4*Math.sin(this.state.time / 50), 
+        totalLevels: 8,
+        size: 200,
+        heightFactor: 0.3 + 0.1*Math.sin(this.state.time)*this.props.sprout + 0.3,
+        lean: 10.0*Math.sin(this.state.time/5)*this.props.sway, 
       }),
       React.createElement('button', {onClick: this.toggle}, buttonText)
     );
@@ -59,49 +59,49 @@ class AnimatedTree extends React.component {
 
 // ===============================================================
 ReactDOM.render(
-  React.createElement(AnimatedTree, {sway: 0.01, sprout: 0.05}),
+  React.createElement(AnimatedTree, {sway: 0.1, sprout: 0.5}),
   document.getElementById('root')
 );
 
-// ==================================
-// Old static tree
-const TreeBox = (props) => {
-  const style = getBoxStyle(props);
-  const baseProps = Object.assign({}, props, {level: props.level + 1});
+// // ==================================
+// // Old static tree
+// const TreeBox = (props) => {
+//   const style = getBoxStyle(props);
+//   const baseProps = Object.assign({}, props, {level: props.level + 1});
   
-  const leftChild = 
-    props.level < props.totalLevels &&
-    React.createElement(TreeBox, 
-      Object.assign({}, baseProps, {right: false})
-    );
+//   const leftChild = 
+//     props.level < props.totalLevels &&
+//     React.createElement(TreeBox, 
+//       Object.assign({}, baseProps, {right: false})
+//     );
 
-  const rightChild =
-    props.level < props.totalLevels &&
-    React.createElement(TreeBox, 
-      Object.assign({}, baseProps, {right: true})
-    );
+//   const rightChild =
+//     props.level < props.totalLevels &&
+//     React.createElement(TreeBox, 
+//       Object.assign({}, baseProps, {right: true})
+//     );
 
-  return React.createElement('div', { style },
-    leftChild,
-    rightChild
-  )
-}
+//   return React.createElement('div', { style },
+//     leftChild,
+//     rightChild
+//   )
+// }
 
-setInterval(() => {
-  time += 1;
+// setInterval(() => {
+//   time += 1;
   
-  ReactDOM.render(
-    React.createElement(TreeBox, { 
-      level: 0,
-      totalLevels: 8,
-      heightFactor: 0.77,
-      lean: 0.2*Math.sin(time / 50), 
-      size: 100,
-    }),
-    document.getElementById('root')
-  );
+//   ReactDOM.render(
+//     React.createElement(TreeBox, { 
+//       level: 0,
+//       totalLevels: 8,
+//       heightFactor: 0.77,
+//       lean: 0.2*Math.sin(time / 50), 
+//       size: 100,
+//     }),
+//     document.getElementById('root')
+//   );
   
-}, 50);
+// }, 50);
   
     
-registerServiceWorker();
+// registerServiceWorker();
